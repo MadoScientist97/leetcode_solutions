59f50663eb0ca415973c0a489a2a40eebe7de3db
MadoScientist97 <sahilthehi3r7@gmail.com>
2022-08-19 20:14:37 +0530

    Water Level

---
+++ med/11.container-with-most-water.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=11 lang=cpp
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (54.12%)
+ * Total Accepted:    1.7M
+ * Total Submissions: 3.1M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * You are given an integer array height of length n. There are n vertical
+ * lines drawn such that the two endpoints of the i^th line are (i, 0) and (i,
+ * height[i]).
+ * 
+ * Find two lines that together with the x-axis form a container, such that the
+ * container contains the most water.
+ * 
+ * Return the maximum amount of water a container can store.
+ * 
+ * Notice that you may not slant the container.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array
+ * [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the
+ * container can contain is 49.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: height = [1,1]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == height.length
+ * 2 <= n <= 10^5
+ * 0 <= height[i] <= 10^4
+ * 
+ * 
+ */
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        if (height.size()==0) return 0;
+        int max=INT_MIN;
+        int l=0,r=height.size()-1;
+        while (l<r) {
+            if (max<min(height[l],height[r])*(r-l))
+                max = min(height[l],height[r])*(r-l);
+            if (height[l] < height[r])
+                l++;
+            else r--;
+        }
+        return max;
+    }
+};
