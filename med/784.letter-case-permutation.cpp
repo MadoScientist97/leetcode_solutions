46525ed1baf02df8f443f7bfbc84f3a7aa7c6bfd
MadoScientist97 <sahilthehi3r7@gmail.com>
2022-09-03 00:24:04 +0530

    784: Letter Case Permutation

---
+++ med/784.letter-case-permutation.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=784 lang=cpp
+ *
+ * [784] Letter Case Permutation
+ *
+ * https://leetcode.com/problems/letter-case-permutation/description/
+ *
+ * algorithms
+ * Medium (72.54%)
+ * Total Accepted:    239.5K
+ * Total Submissions: 327K
+ * Testcase Example:  '"a1b2"'
+ *
+ * Given a string s, you can transform every letter individually to be
+ * lowercase or uppercase to create another string.
+ * 
+ * Return a list of all possible strings we could create. Return the output in
+ * any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "a1b2"
+ * Output: ["a1b2","a1B2","A1b2","A1B2"]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "3z4"
+ * Output: ["3z4","3Z4"]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 12
+ * s consists of lowercase English letters, uppercase English letters, and
+ * digits.
+ * 
+ * 
+ */
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        List<StringBuilder> ans = new ArrayList();
+        ans.add(new StringBuilder());
+
+        for (char c: S.toCharArray()) {
+            int n = ans.size();
+            if (Character.isLetter(c)) {
+                for (int i = 0; i < n; ++i) {
+                    ans.add(new StringBuilder(ans.get(i)));
+                    ans.get(i).append(Character.toLowerCase(c));
+                    ans.get(n+i).append(Character.toUpperCase(c));
+                }
+            } else {
+                for (int i = 0; i < n; ++i)
+                    ans.get(i).append(c);
+            }
+        }
+
+        List<String> finalans = new ArrayList();
+        for (StringBuilder sb: ans)
+            finalans.add(sb.toString());
+        return finalans;
+    }
+}
