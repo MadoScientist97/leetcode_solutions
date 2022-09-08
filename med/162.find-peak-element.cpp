ceb8c6c2b2a952514d51cd408719de39a00773b1
MadoScientist97 <sahilthehi3r7@gmail.com>
2022-09-08 15:09:54 +0530

    162: Find peak Element (Binary Search)

---
+++ med/162.find-peak-element.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=162 lang=cpp
+ *
+ * [162] Find Peak Element
+ *
+ * https://leetcode.com/problems/find-peak-element/description/
+ *
+ * algorithms
+ * Medium (46.00%)
+ * Total Accepted:    877.2K
+ * Total Submissions: 1.9M
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * A peak element is an element that is strictly greater than its neighbors.
+ * 
+ * Given a 0-indexed integer array nums, find a peak element, and return its
+ * index. If the array contains multiple peaks, return the index to any of the
+ * peaks.
+ * 
+ * You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is
+ * always considered to be strictly greater than a neighbor that is outside the
+ * array.
+ * 
+ * You must write an algorithm that runs in O(log n) time.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index
+ * number 2.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,2,1,3,5,6,4]
+ * Output: 5
+ * Explanation: Your function can return either index number 1 where the peak
+ * element is 2, or index number 5 where the peak element is 6.
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 1000
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * nums[i] != nums[i + 1] for all valid i.
+ * 
+ * 
+ */
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int size = nums.size()-1;
+        if (size == -1) return -1;
+        if (size == 0) return 0;
+        if (size == 1) return nums[0]>nums[1]?0:1;
+        int lp=0,rp=size;
+        while (lp<=rp) {
+            int mid = (lp+rp)/2;
+            if (mid>0 && mid<size ) {
+                if (nums[mid-1] < nums[mid] && nums[mid+1] < nums[mid] ) {
+                    return mid;
+                }
+                else if (nums[mid-1] > nums[mid]) {
+                    rp = mid;
+                }
+                else lp = mid+1;
+            }
+            else {
+                return mid;
+            }
+       }
+       return lp;
+    }
+};
