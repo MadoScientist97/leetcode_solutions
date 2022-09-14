7bbff78624f97b369cb39384954d5dfa5b0be3b4
MadoScientist97 <sahilthehi3r7@gmail.com>
2022-09-14 22:45:44 +0530

    21: Merge Two Sorted List

---
+++ easy/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (60.47%)
+ * Total Accepted:    2.6M
+ * Total Submissions: 4.2M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * 
+ * Merge the two lists in a one sorted list. The list should be made by
+ * splicing together the nodes of the first two lists.
+ * 
+ * Return the head of the merged linked list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *p1 = list1, *p2 = list2, *head = nullptr;
+        if (!p1) return p2; if (!p2) return p1;
+        if (p1->val < p2->val) { head = p1; p1 = p1->next;} else {head = p2; p2=p2->next;}
+        ListNode *iter = head;
+        while (p1!=nullptr && p2!=nullptr) {
+            if (p1->val < p2->val) {
+                iter->next = p1;
+                p1 = p1->next;
+            }
+            else {
+                iter->next = p2;
+                p2 = p2->next;
+            }
+            iter = iter->next;
+        }
+        if (p1) {iter->next = p1;}
+        if (p2) {iter->next = p2;}
+        return head;
+    }
+};
