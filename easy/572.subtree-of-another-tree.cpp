24e633e4c55baf8e3ae1ffe33aa2b310183f7893
MadoScientist97 <sahilthehi3r7@gmail.com>
2022-09-21 23:34:58 +0530

    572: Subtree of another tree.

---
+++ easy/572.subtree-of-another-tree.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode id=572 lang=cpp
+ *
+ * [572] Subtree of Another Tree
+ *
+ * https://leetcode.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (45.42%)
+ * Total Accepted:    560.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * Given the roots of two binary trees root and subRoot, return true if there
+ * is a subtree of root with the same structure and node values of subRoot and
+ * false otherwise.
+ * 
+ * A subtree of a binary tree tree is a tree that consists of a node in tree
+ * and all of this node's descendants. The tree tree could also be considered
+ * as a subtree of itself.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the root tree is in the range [1, 2000].
+ * The number of nodes in the subRoot tree is in the range [1, 1000].
+ * -10^4 <= root.val <= 10^4
+ * -10^4 <= subRoot.val <= 10^4
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSame(TreeNode* root,TreeNode* subRoot){
+        if(root==NULL && subRoot==NULL){
+            return true;
+        }
+        if(root==NULL || subRoot==NULL){
+            return false;
+        }
+        else if(root->val!=subRoot->val){
+            return false;
+        }
+        bool leftTree=isSame(root->left,subRoot->left);
+        bool rightTree=isSame(root->right,subRoot->right);
+        return leftTree && rightTree;
+    }
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root==NULL){
+            return false;
+        }
+        if(isSame(root,subRoot)){
+            return true;
+        }
+        bool leftTree=isSubtree(root->left,subRoot);
+        bool rightTree=isSubtree(root->right,subRoot);
+        
+        return leftTree || rightTree;
+    }
+};
